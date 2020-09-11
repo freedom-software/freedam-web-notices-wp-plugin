7534def2fa1d0f4e045df56bca3da72bf86a7a5e
Aidan Dunn <aidancheyd@gmail.com>
2020-09-11 12:29:24 +1200

    feat(admin): adds partial for writing instructions in

---
+++ freedam-web-notices/admin/partials/freedam-web-notices-admin-instructions.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a set of instructions for using the admin settings
+ *
+ * This file is used to markup the admin-facing section for instructions
+ *
+ * @link       https://github.com/freedom-software
+ * @since      1.1
+ *
+ * @package    Freedam_Web_Notices
+ * @subpackage Freedam_Web_Notices/admin/partials
+ */
+?>
+
+Some Instructions
